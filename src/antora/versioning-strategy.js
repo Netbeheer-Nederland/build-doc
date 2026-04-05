07a2733a4c28690d3c89aa26978d60241675c4fb
Bart Kleijngeld <bartkl@gmail.com>
2026-04-05 22:00:00 +0200

    Trying out new version of release strategy

--- src/antora/versioning-strategy.js
+++ src/antora/versioning-strategy.js
@@ -1,14 +1,18 @@
 module.exports.register = function () {
   this.once("contentAggregated", ({ contentAggregate }) => {
     for (const bucket of contentAggregate) {
-      switch (process.env.VERSIONING_STRATEGY) {
+      switch (process.env.RELEASE_STRATEGY) {
         case "release":
           if (!/^\d+\.\d+$/.test(bucket.version))
             bucket.prerelease = true
+          else if (bucket.version == "main")
+	    bucket.version = "next"
         break
         case "evolution":
           if (bucket.version != "main")
             bucket.prerelease = true
+          else
+            bucket.version = "current"
         break
       }
     }
