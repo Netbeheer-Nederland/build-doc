0c86e3a886b1c7ed43b57ecea2e094d94ef2d6b3
Bart Kleijngeld <bartkl@gmail.com>
2026-04-06 14:08:26 +0200

    Fixed version postprocessing

--- src/antora/version-postprocessor.js
+++ src/antora/version-postprocessor.js
@@ -1,19 +1,22 @@
 module.exports.register = function () {
   this.once("contentAggregated", ({ contentAggregate }) => {
     for (const bucket of contentAggregate) {
-      console.log("my object: %o", bucket)
       switch (process.env.RELEASE_STRATEGY) {
         case "release":
-          if (!/^\d+\.\d+$/.test(bucket.version))
+          if (!/^\d+\.\d+$/.test(bucket.version)) {
             bucket.prerelease = true
-          else if (bucket.version == "main")
+	  }
+          if (bucket.version == "main") {
 	    bucket.version = "next"
+	  }
         break
         case "evolution":
-          if (bucket.version == "main")
+          if (bucket.version == "main") {
             bucket.version = "current"
-          else
+	  }
+          else {
             bucket.prerelease = true
+	  }
         break
       }
     }
