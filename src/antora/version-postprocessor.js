54f1017656b48dfa89d1893fe5bb96846dec93e0
Bart Kleijngeld <bartkl@gmail.com>
2026-04-06 15:15:44 +0200

    Better display versions for drafts

--- src/antora/version-postprocessor.js
+++ src/antora/version-postprocessor.js
@@ -1,6 +1,12 @@
 module.exports.register = function () {
   this.once("contentAggregated", ({ contentAggregate }) => {
     for (const bucket of contentAggregate) {
+      /* Draft branches. */
+      if (/^draft-/.test(bucket.version)) {
+        const [_, workName] = /^draft-(.*)$/.exec(bucket.version)
+        bucket.displayVersion = `${workName} (draft)`
+      }
+
       switch (process.env.RELEASE_STRATEGY) {
         case "release":
           if (!/^\d+\.\d+$/.test(bucket.version)) {
@@ -8,11 +14,13 @@ module.exports.register = function () {
 	  }
           if (bucket.version == "main") {
 	    bucket.version = "upcoming"
+            bucket.displayVersion = "upcoming"
 	  }
         break
         case "evolution":
           if (bucket.version == "main") {
             bucket.version = "current"
+            bucket.displayVersion = "current"
 	  }
           else {
             bucket.prerelease = true
